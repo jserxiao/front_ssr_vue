a074fcdce3c3df65c72d4c1dfe9fa5fb367b4104
肖佳坤 <xiao@xiaojiakundeMacBook-Pro.local>
2024-05-28 20:44:25 +0800

    feat: route

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="index">
-     <NuxtWelcome/>
+      <NuxtWelcome/>
+      <span @click="link('home')">home</span>
+      <span @click="link('login')">login</span>
     </div>
   </template>
+<script setup lang="ts">
+const {push} = useRouter();
+const link = (path: string) => {
+  push(path)
+}
+</script>
